7cdc63cbd16d97857a1f5ecfd60338590f1851a3
dauzic99 <dauzic99@gmail.com>
2023-04-26 16:55:41 +0800

    fix asset, change login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,89 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.authentication.master')
+@section('title', 'Login-bs-validation')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('css')
+@endsection
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('style')
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-7 order-1"><img class="bg-img-cover bg-center" src="{{ asset('assets/images/login/1.jpg') }}"
+                    alt="looginpage"></div>
+            <div class="col-xl-5 p-0">
+                <div class="login-card">
+                    <div>
+                        <div>
+                            <a class="logo text-start" href="{{ route('Dashboard') }}"><img class="img-fluid for-light"
+                                    src="{{ asset('assets/images/logo/login.png') }}" alt="looginpage"><img
+                                    class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}"
+                                    alt="looginpage"></a>
+                        </div>
+                        <div class="login-main">
+                            <form class="theme-form needs-validation" novalidate="" action="{{ route('login') }}"
+                                method="POST">
+                                @csrf
+                                <h4>Sign in to account</h4>
+                                <p>Enter your email & password to login</p>
+                                <div class="form-group">
+                                    <label class="col-form-label">Email Address</label>
+                                    <input class="form-control" type="email" required="" placeholder="Test@gmail.com"
+                                        name="email">
+                                    <div class="invalid-feedback">Please enter proper email.</div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-form-label">Password</label>
+                                    <input class="form-control" type="password" name="password" required=""
+                                        placeholder="*********">
+                                    <div class="invalid-feedback">Please enter password.</div>
+                                    <div class="show-hide"><span class="show"> </span></div>
+                                </div>
+                                <div class="form-group mb-0">
+                                    <div class="checkbox p-0">
+                                        <input id="checkbox1" type="checkbox">
+                                        <label class="text-muted" for="checkbox1">Remember password</label>
+                                    </div>
+                                    <button class="btn btn-primary btn-block" type="submit">Sign in</button>
+                                </div>
+                                <h6 class="text-muted mt-4 or">Or Sign in with</h6>
+                                <div class="social mt-4">
+                                    <div class="btn-showcase"><a class="btn btn-light" href="https://www.linkedin.com/login"
+                                            target="_blank"><i class="txt-linkedin" data-feather="linkedin"></i> LinkedIn
+                                        </a><a class="btn btn-light" href="https://twitter.com/login?lang=en"
+                                            target="_blank"><i class="txt-twitter" data-feather="twitter"></i>twitter</a><a
+                                            class="btn btn-light" href="https://www.facebook.com/" target="_blank"><i
+                                                class="txt-fb" data-feather="facebook"></i>facebook</a></div>
+                                </div>
+
+                                <script>
+                                    (function() {
+                                        'use strict';
+                                        window.addEventListener('load', function() {
+                                            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                                            var forms = document.getElementsByClassName('needs-validation');
+                                            // Loop over them and prevent submission
+                                            var validation = Array.prototype.filter.call(forms, function(form) {
+                                                form.addEventListener('submit', function(event) {
+                                                    if (form.checkValidity() === false) {
+                                                        event.preventDefault();
+                                                        event.stopPropagation();
+                                                    }
+                                                    form.classList.add('was-validated');
+                                                }, false);
+                                            });
+                                        }, false);
+                                    })();
+                                </script>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+@endsection
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@section('script')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('pages.index');
+})->middleware(['auth', 'verified'])->name('Dashboard');
 
 Route::middleware('auth')->group(function () {
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
